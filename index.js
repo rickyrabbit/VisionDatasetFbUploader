83b62660f52aa86d1922e4afb6a699cf14248dbd
Unknown <riccardoconiglio@gmail.com>
2018-04-30 11:14:40 +0200

    added videoArgs and loop
    Added videoArgs parameter,for loop cycle based on count query and improved documentation

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+/**
+ * This project needs dotenv module to initialize
+ * facebook tokens and mongoDB credentials to access the database
+ * Data is stored in .env file in project folder
+ */
 require('dotenv').config();
 
+/**
+ * fs and path module are needed to handle file system and resolve video/images paths
+ */
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); 
+
+/**
+ * facebook-api-video upload is a npm module that handles video uploading with Graph API calls
+*/
 
 const fbUpload = require('facebook-api-video-upload');
+
+/**
+ * mongoose is a npm module that handles CRUD operations with a MongoDB database entity
+ */
 const mongoose = require("mongoose");
 
-const env_debug = true;
+const env_debug = true; //flag used to debug
 
-/*FACEBOOK*/
+/** FACEBOOK
+ *  All Facebook constants are initialized and stored in the .env file
+*/
 
 const group_VisionDataset_id    =process.env.FB_GROUP_ID;
 const page_VisionDataset_id     =process.env.FB_PAGE_ID;
@@ -17,7 +35,23 @@ const profile_id                =process.env.FB_PROFILE_ID;
 const fbToken                   =process.env.FB_USER_TOKEN;
 const fbToken_page              =process.env.FB_PAGE_TOKEN;
 
-const personalPath              =process.env.VISION_PATH;
+/**
+ * @param {const} personalPath          Holds the path to Vision dataset on the machine
+ */
+const personalPath              =process.env.VISION_PATH;   
+
+
+/**
+ * @function uploadHandler              Async function that uploads a video stored on the machine based on the database query
+ * 
+ * @param {JSON Object} doc             Holds the data queried on the database
+ * @param {const} filename              Name with extension of the video file to upload
+ * @param {const} filePath              Path to video file to upload
+ * @param {const} path                  User specific path to the video to upload 
+ * @param {const} args                  Facebook settings for a single video upload
+ * 
+ * @return {Promise}                    Promise that contains the upload success state and facebook video id in case of a successful upload 
+ */
 
 async function uploadHandler(doc){
     const filename  = doc.inputfilename+'.'+doc.extension;
@@ -26,21 +60,26 @@ async function uploadHandler(doc){
     const path      = personalPath+filePath;
     if(env_debug){console.log('path: %s',path)};
 	const args = {
-		token: fbToken_page, // with the permission to upload
-		id: page_VisionDataset_id, //The id represent {page_id || user_id || event_id || group_id}
-		stream: fs.createReadStream(path), //path to the video,
-		title: path,
-		description: filePath
+		token: fbToken_page,                //  Facebook token with upload permissions
+		id: page_VisionDataset_id,          //  The id represent {page_id || user_id || event_id || group_id}
+		stream: fs.createReadStream(path),  //  Path to the video,
+		title: path,                        //  Video title on Facebook
+		description: filePath               //  Video description on Facebook
 	};
 
     //const result = await fbUpload(args);
     const result = fbUpload(args);
 	return result;
-
 }
 
-/*FACEBOOK*/
-/*MONGODB*/
+/**MONGODB
+ * 
+ * @param {string} uri                  Uri that ensures database shell conncetion
+ * @param {mongoose.Schema} mediaSchema Schema of typical database object(document in mongoDB)
+ * @param {mongoose.model}  mediainfo   Model of typical document in "videos" collection
+*/
+//TODO check if version of MongoDB cluster updated to 3.6 @high 
+//see https://docs.atlas.mongodb.com/driver-connection/#node-js-driver-example
 var uri ='mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0-shard-00-00-cpfag.mongodb.net:27017,cluster0-shard-00-01-cpfag.mongodb.net:27017,cluster0-shard-00-02-cpfag.mongodb.net:27017/'+process.env.DB_NAME+'?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
 const mediaSchema =new mongoose.Schema({
@@ -61,6 +100,8 @@ const ext_3gp       = { extension: '3gp' };
 const ext_mov       = { extension: 'mov' };
 const ext_mp4       = { extension: 'mp4' };
 
+
+// List of tipologies of media present in the database
 const mediatipology_nat         = { mediatipology: 'nat'        };
 const mediatipology_natFBH      = { mediatipology: 'natFBH'     };
 const mediatipology_natFBL      = { mediatipology: 'natFBL'     };
@@ -75,6 +116,10 @@ const mediatipology_outdoor     = { mediatipology: 'outdoor'    };
 const mediatipology_outdoorWA   = { mediatipology: 'outdoorWA'  };
 const mediatipology_outdoorYT   = { mediatipology: 'outdoorYT'  };
 
+/**
+ * @function dbState                Function that query the state of the database 
+ * @return                          State of the database
+ */
 function dbState(num){
     switch(num){
         case 0:
@@ -88,23 +133,41 @@ function dbState(num){
     }
 }
 
+/**
+ * @function countQuery             Async function that counts the number of documents in the query
+ * @param {String} q_extension      Query filtered with this extension
+ * @param {String} q_mediaTipology  Query filtered with this mediatipology
+ * 
+ * @returns                         Number of documents
+ */
 async function countQuery(q_extension,q_mediaTipology){
     let query = mediainfo.where(notUploaded).where(q_extension);
     if (q_mediaTipology!='allmt') {
         query = query.and(q_mediaTipology);
     }
-    const countD = await query.count().exec();
+    const countD = await query.count().exec();  //Await resolves the promise
     if(env_debug){console.log('countQuery => %s',countD )};
     return countD;
 }
 
+/**
+ * @function attemptCloseDb         Function that tries to close the db and logs it with a message
+ * @param {String} message          Message to be reported to the console
+ */
 async function attemptCloseDb(message){
         //if(env_debug){console.log('Nothing to upload!')};
         if(env_debug){console.log('db %s closing in %s',process.env.DB_NAME,message)};
-        await mongoose.connection.close();
+        await mongoose.connection.close();  //awaits the closing of the database
         if(env_debug){console.log('db %s %s in %s',process.env.DB_NAME,dbState(mongoose.connection.readyState),message)};
 }
 
+/**
+ * @function findOne                Async function that finds one document with selected filters
+ * @param {String} q_extension      Query filtered with this extension
+ * @param {String} q_mediaTipology  Query filtered with this mediatipology
+ * 
+ * @return {JSON Object}            Queried document with this filters
+ */
 async function findOne(q_extension,q_mediaTipology){
     let query = mediainfo.where(notUploaded).where(q_extension);
     if (q_mediaTipology!='allmt') {
@@ -115,56 +178,78 @@ async function findOne(q_extension,q_mediaTipology){
     return doc;
 }
 
+/**
+ * @function updateOne              Async function that updates one selected document in the database with his facebook id
+ * 
+ * @param {String} doc_id           Document unique id 
+ * @param {String} fb_video_id      Facebook video id 
+ * @return {JSON Object}            Status regarding update operation
+ */
 async function updateOne(doc_id,fb_video_id){
     let query   = mediainfo.where({_id:doc_id});
-    query       = query.update({ $set:{ fbsource: fb_video_id ,uploaded: 'false'}});
+    query       = query.update({ $set:{ fbsource: fb_video_id ,uploaded: 'true'}});
     const result =  await query.exec();
     if(env_debug){console.log('updateOne => %s',result )};
     return result;
 }
 
-/*MONGODB*/
+//Actual program execution
+
 console.clear();
-if(env_debug){console.log('debug')};
+if(env_debug){console.log('debug mode ON')};
+
+let videoArgs ={
+    extension:ext_mp4,
+    mediatip:mediatipology_flat,
+}
 
 let counts=0;
-// for loop exists if counts >0
+// for loop cycles if counts >0
 //database is closed here
 mongoose.connect(uri);
 
-countQuery(ext_mp4,mediatipology_flat).then((countD) => {
+
+countQuery(videoArgs.extension,videoArgs.mediatip).then((countD) => {
     counts=countD;
     if(env_debug){console.log('counts => %s',counts)};
     attemptCloseDb('countQuery');
 });
 let dbDoc,resUpload,resUpdate,mDoc,mResUpl,mResUpd;
-//for (let index = counts; index >0 ;) {
+//TODO implement for cycle @critical
+for (let index = counts; index >0 ;) {
     //if(env_debug){console.log('index is: %s',index)};
     mongoose.connect(uri);
-    dbDoc=findOne(ext_mp4,mediatipology_flat);
+    dbDoc=findOne(videoArgs.extension,videoArgs.mediatip);
     dbDoc.then((doc) => {
+        console.log(`There are ${index} remaining videos to upload`);
         mDoc=doc[0];
         if(env_debug){console.log('Using Object with id : %s',mDoc._id)};
         mResUpl=uploadHandler(mDoc);
         mResUpl.then((result) => {
             resUpload=result;
-            if(env_debug){console.log('Success? : %s , Video id: %s',resUpload.success,resUpload.video_id)};
-            if(env_debug){console.log('resUpload  %s',resUpload)};
-            if(env_debug){console.log('success    %s',resUpload.success)};
+            if(env_debug){
+                console.log('Success? : %s , Video id: %s',resUpload.success,resUpload.video_id);
+                console.log('resUpload  %s',resUpload);
+                console.log('success    %s',resUpload.success);
+            }
             if(resUpload.success==false){
                 throw new Error('Upload failed');
             }
             if(resUpload.success==true){
-                if(env_debug){console.log('Database Update phase');}
-                if(env_debug){console.log('db state:  %s %s',process.env.DB_NAME,dbState(mongoose.connection.readyState))};
+                if(env_debug){
+                    console.log('Database Update phase');
+                    console.log('db state:  %s %s',process.env.DB_NAME,dbState(mongoose.connection.readyState));
+                }
                 mongoose.connect(uri);
                 mResUpd=updateOne(mDoc._id,resUpload.video_id);
                 mResUpd.then((result) =>{
                     resUpdate=result;
                     if(env_debug){console.log('ok? : %s , n: %s',resUpdate.ok,resUpdate.n)};
                     if(resUpdate.ok==1 && resUpdate.n==1){
-                        if(env_debug){console.log('Update has been done correctly')};
-                        //index--;
+                        if(env_debug){
+                            console.log('Update has been done correctly')
+                        };
+                        if(env_debug){console.log(`Index value : ${index--}`);}
                         attemptCloseDb('Closing db after check');  
                     }
                     //  
@@ -176,4 +261,4 @@ let dbDoc,resUpload,resUpdate,mDoc,mResUpl,mResUpd;
     
     
 
-//}
+}//for loop parenthesis
